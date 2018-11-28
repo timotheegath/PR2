8325c13feddbb363aadbad24d6fd7dda67e3eaf7
Luka <luka.lagator15@imperial.ac.uk>
2018-11-28 14:52:53 +0000

    ij

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-import numpy AS NP
+import numpy as np
+import data_in_out as io
 
+f = io.load_features()
+features = np.memmap('features', mode='w+', shape=f.shape)
+features[:] = f[:]
+print(features.shape)
 
