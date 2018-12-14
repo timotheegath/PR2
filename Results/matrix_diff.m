2cf3f0d71732cbb5f2509fc01eabeec2828e2e03
timotheegath <timogath@mail.com>
2018-12-14 22:15:56 +0000

    dfghjk

--- Results/matrix_diff.m
+++ Results/matrix_diff.m
@@ -3,7 +3,9 @@ NK = load('maha_I_init_train_param_');
 RBF = load('RBF_maha_I_init_train_param_');
 Poly = load('poly_maha_I_init_train_param_');
 
-diff = squeeze(NK.L(1, :, :)) - squeeze(RBF.L(1, :, :));
-diff(logical(eye(size(diff, 1)))) = 0;
+diff = squeeze(NK.L(1, :, :))*squeeze(NK.L(1, :, :))' - ...
+squeeze(RBF.L(1, :, :))*squeeze(RBF.L(1, :, :))';
+
+%diff(logical(eye(size(diff, 1)))) = 0;
 imagesc(abs(diff))
 colorbar
